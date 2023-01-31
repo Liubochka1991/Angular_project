0e96b10277eec4d14f49dc9c6a372c7977f1d67e
Liubochka <rysaja1412@gmail.com>
2023-01-31 19:32:02 +0200

    three commit

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -12,6 +13,14 @@ email:String='rysaja@gmail.com';
 home:String='Чернівці';
 experience:Number=2;
 photo:Object='../../assets/photo.jpg';
-payment:number=1000
-apply:number=0.8
+payment:number=1000;
+apply:number=0.8;
+hobby:Array<any>=[
+  {id:1, name:"Малювання"},
+  {id:2, name:"Комп'ютерні ігри"},
+  {id:3, name:"Гра в шахи"}
+];
+bmw:String='bmw';
+audi:String='audi';
+mersedes:String='mersedes'
 }
